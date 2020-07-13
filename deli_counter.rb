2f745f077a5c77209f9a04a98c748702d8e7e65a
Justin Lupica <justinlupica@gmail.com>
2020-07-13 18:18:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.length >= 1
+    sentence1 = "The line is currently:"
+    katz_deli.each.with_index(0) do |people, index|
+      index += 1
+      sentence1 << " #{index}. #{people}"
+    end
+    puts "#{sentence1}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  counter = 1
+    katz_deli.each_with_index do |name|
+    puts "Welcome, #{name}. You are number #{counter} in line."
+    counter += 1
+  end
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
+end
+end
